e30552614c1a8154c287f96d712eda97500d599b
AmirMohammadiKarbalaei <a.mohammadikarbalaei@gmail.com>
2023-08-01 10:29:34 +0100

    First push

--- BBC_daily_links_save_postgre_script.py
+++ BBC_daily_links_save_postgre_script.py
@@ -15,24 +15,22 @@ namespaces = {
     'news': 'http://www.google.com/schemas/sitemap-news/0.9'
 }
 
-
-
-
 BBC_today_urls = Extract_todays_urls_from_BBC_sitemap(BBC_news_sitemaps,namespaces)
+
+
 data_frame_BBC = pd.DataFrame.from_dict(BBC_today_urls, orient='index')
-
 data_frame_BBC["Url"]= data_frame_BBC.index
-
 data_frame_BBC.reset_index(drop=True, inplace=True)
 
-##Removing none english entries
 
+##Removing none english entries
 for idx,title in  enumerate(data_frame_BBC.Title):
     if not is_english_sentence(title):
         data_frame_BBC.drop(idx,inplace=True)
 
 data_frame_BBC['Topic'] = data_frame_BBC['Url'].str.split('com/').str[1].str.split('/').str[0]
 topics_to_drop = ["pidgin", "hausa", "swahili", "naidheachdan"]
+
 data_frame_BBC.drop(data_frame_BBC[data_frame_BBC['Topic'].isin(topics_to_drop)].index, axis=0, inplace=True)
 data_frame_BBC.reset_index(drop=True,inplace=True)
 
@@ -48,10 +46,6 @@ data_frame_BBC["Article content"] = all_content
 
 
 
-
-
-
-
 ##database credentials
 db_credentials = {
     'dbname': 'Web_Extention',
@@ -62,19 +56,15 @@ db_credentials = {
 }
 
 
-# Establish a connection to the database
 connection = psycopg2.connect(**db_credentials)
 
-# Create a cursor to interact with the database
-cursor = connection.cursor()
-
-# Create a cursor to interact with the database
+# Creating a cursor to interact with the database
 cursor = connection.cursor()
 for _, row in data_frame_BBC.iterrows():
     insert_query = "INSERT INTO bbc_daily_links (last_modified, title, url, topic,article_content) VALUES (%s, %s, %s, %s, %s);"
     cursor.execute(insert_query, (row["Last Modified"], row["Title"], row["Url"], row["Topic"], row["Article content"]))
 
-# Commit the changes and close the connection
+
 connection.commit()
 cursor.close()
 connection.close()
